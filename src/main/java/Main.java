62b87c12d509c7875ee408f34cd7b15b9a35ba09
“inna.savchenko” <inna.savchenko@tufin.com>
2022-08-07 21:37:50 +0300

    Fixing DB connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,10 @@ public class Main {
         Game g = new CheckersGame();
         Board b = new Board();
 
-        String addUser = "INSERT INTO users VALUES(2,'Meirav','98745',0)";
-        //Database db = new Database();
+        //String addUser = "INSERT INTO users VALUES(4,'Tamar','23587',0)";
+        Database db = new Database();
         //db.execution(addUser);
+        System.out.println(db.toString());
 
 
     }
