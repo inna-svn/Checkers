b42978f183f569956ca232e5fe0725aefe4dbcb0
“inna.savchenko” <inna.savchenko@tufin.com>
2022-08-20 23:59:15 +0300

    handle signIn and signUp: add new user, check if user already exists

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,24 +1,20 @@
 import java.sql.*;
-import java.util.LinkedList;
-import java.util.List;
+
 
 public class Database {
     private Connection connection = null;
     private Statement statement = null;
-    private static String url = "jdbc:mysql://localhost:3306/checkers";
+    private static final String url = "jdbc:mysql://localhost:3306/checkers";
     private static Database database = null;
 
     public Database() {
         try {
-            connection = DriverManager.getConnection(url,"root","");
-            connection.setAutoCommit(false);
+            connection = DriverManager.getConnection(url,"root","zubur1");
+            connection.setAutoCommit(true);
             statement = connection.createStatement();
-            connection.commit();
-        } catch (
-                SQLException sqlException) {
+        } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         }
-
     }
 
     public static Database getDatabase(){
@@ -35,71 +31,5 @@ public class Database {
         return connection;
     }
 
-    public String getConnectionStatus(){
-        String connected = "Connection to DB is opened.";
-        boolean closed = true;
-        try{
-            closed = connection.isClosed();
-        } catch(
-                SQLException sqlException) {
-            sqlException.printStackTrace();
-        }
-        if (!closed)
-            return connected;
-        return "Connection to DB is closed.";
-    }
-
-    public ResultSet execQuery(String query, Database db) throws IllegalAccessException, SQLException {
-        try {
-            Statement s;
-            ResultSet rs;
-            Connection conn = null;
-            conn = getConnection();
-            s = conn.createStatement();
-            rs = s.executeQuery(query);
-            return rs;
-
-        } finally {
-    //        connection.close();
-        }
-    }
-
-    public int executeQuery(String query) throws Exception {
-        int res = 0;
-        try {
-            ResultSet rs = execQuery(query,database);
-            ResultSetMetaData rsmd = rs.getMetaData();
-            if (rs.next())
-                res = 1;
-          //  rs.close();
-        //    connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return res;
-    }
-
-    public int updateQuery(String query) throws Exception {
-        Statement s;
-        Connection conn = getConnection();
-        s = conn.createStatement();
-        int result = s.executeUpdate(query);
-        return result;
-    }
-
-    public void execution(String query){
-        try {
-            getStatement().execute(query);
-            getConnection().commit();
-        } catch (
-                SQLException sqlException) {
-            sqlException.printStackTrace();
-        }
-
-    }
-
-    public String toString() {
-        return url + "\n" + getConnectionStatus();
-    }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,26 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Main {
+
     public static void main(String[] args) {
-        User activeUser = new User("inna");
-        User inactiveUser = new User("ilya");
+//        try {
+//            User activeUser = new User("inna");
+//            activeUser=User.signIn("Meirav", "98745");
+//            System.out.println(activeUser.toString());
+//        } catch (User.SignInError signInError) {
+//            signInError.printStackTrace();
+//        }
+//        User newUser = new User("");
+//        try {
+//            newUser=User.signUp("new","123");
+//        } catch (User.SignUpError signUpError) {
+//            signUpError.printStackTrace();
+//        }
         Game g = new CheckersGame();
-        g.start(activeUser,inactiveUser);
         Board b = new Board();
-
-        String updateUser = "UPDATE users SET userName='Tamara' where id=1";
-        String selectQuery = "select * from users where userName='Tamaraffff';";
-        String selectQueryNegativeTesting = "select * from users where userName='Tamara';";
-
         Database db = new Database();
-        try {
-        db.updateQuery(updateUser);
-        System.out.println("result from select query is : " +db.executeQuery(selectQuery));
-        System.out.println("result from select query is : " +db.executeQuery(selectQueryNegativeTesting));
-
-            //     System.out.println(""+db.executeQuery(updateUser));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        System.out.println(db);
-
-
     }
+
 }
