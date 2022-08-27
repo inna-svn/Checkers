18c97ab23cc3ca60febd8f350a2d2a2e5397ba08
Ilya Sher P <34807039+ilyash-b@users.noreply.github.com>
2022-08-27 17:48:15 +0400

    Add tests dummy

---
+++ src/main/java/CheckersGameTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckersGameTest {
+
+    @org.junit.jupiter.api.Test
+    void listPossibleMoves() {
+        // setup pieces
+        // listPossibleMoves()
+        // check result
+    }
+
+    @org.junit.jupiter.api.Test
+    void makeMove() {
+    }
+}
