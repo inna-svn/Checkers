d647fbfdbce20b8ee48a3be772498ab502ffe5a5
Ilya Sher P <34807039+ilyash-b@users.noreply.github.com>
2022-09-10 12:15:51 +0400

    WIP: web - game page

--- src/main/java/Location.java
+++ src/main/java/Location.java
@@ -2,6 +2,20 @@ import com.google.common.base.Preconditions;
 
 // Was not in project plan
 public record Location(int x, int y) {
+
+    enum Color {
+        WHITE,
+        BLACK
+    }
+
+    public Location.Color getColor() {
+        if ((x + y) % 2 == 0) {
+            return Color.WHITE;
+        } else {
+            return Color.BLACK;
+        }
+    }
+
     public Location {
         Preconditions.checkArgument(x >= 0 && x < 8);
         Preconditions.checkArgument(y >= 0 && y < 8);
